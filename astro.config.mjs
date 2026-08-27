36c1bd02a8550e29a4887961f434f2920216af3f
daidarz <davidandreica127@gmail.com>
2026-08-27 16:38:39 +0200

    web0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  site: "https://daidarzzz.github.io",
+  base: "/indexly/",
 });
